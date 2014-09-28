2ce3c99160e2199300a1527a1ec330a31b8f07ee
Matthieu Moy <Matthieu.Moy@imag.fr>
2014-09-28 16:00:20 +0200

    License

--- piwigo-random-js.php
+++ piwigo-random-js.php
@@ -1,4 +1,24 @@
 <?php // -*- mode:php c-basic-offset: 2 -*-
+// +-----------------------------------------------------------------------+
+// | Piwigo RANDOM - Insert images from a Piwigo Galery in a website       |
+// +-----------------------------------------------------------------------+
+// | Copyright(C) 2014 Matthieu Moy                                        |
+// +-----------------------------------------------------------------------+
+// | This program is free software; you can redistribute it and/or modify  |
+// | it under the terms of the GNU General Public License as published by  |
+// | the Free Software Foundation                                          |
+// |                                                                       |
+// | This program is distributed in the hope that it will be useful, but   |
+// | WITHOUT ANY WARRANTY; without even the implied warranty of            |
+// | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU      |
+// | General Public License for more details.                              |
+// |                                                                       |
+// | You should have received a copy of the GNU General Public License     |
+// | along with this program; if not, write to the Free Software           |
+// | Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, |
+// | USA.                                                                  |
+// +-----------------------------------------------------------------------+
+
 // Your piwigo gallery here:
 $site = "http://mmoy.piwigo.com/";
 $maximages = 1;

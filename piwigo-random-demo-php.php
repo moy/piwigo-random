ee882f5ecd42f47ab59310677345e2942e4326f9
Matthieu Moy <Matthieu.Moy@imag.fr>
2015-10-04 10:41:55 +0200

    Add target=... tests in demo

--- piwigo-random-demo-php.php
+++ piwigo-random-demo-php.php
@@ -14,7 +14,13 @@
       <?php $base_url = "http://www-verimag.imag.fr/~moy/piwigo-random/piwigo-random-backend.php";
 	     echo file_get_contents($base_url . "?mode=html&cat_id=13"); ?>
     is a random image</p>
-    
+
+    <h1>Using file_get_content and setting the target</h1>
+    <p>This
+      <?php $base_url = "http://www-verimag.imag.fr/~moy/piwigo-random/piwigo-random-backend.php";
+	     echo file_get_contents($base_url . "?mode=html&target=_self"); ?>
+    is a random image</p>
+
     <div id="validator">
       <a href="http://validator.w3.org/check?uri=referer">
 	<img src="http://www.w3.org/html/logo/downloads/HTML5_Logo_64.png"
